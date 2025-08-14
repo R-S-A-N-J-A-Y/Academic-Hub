4e08c25d52826424881c76843b5f31773b73c925
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-08-14 20:18:27 +0530

    Created all required boiler page with navigation and included sample db schema

--- Frontend/src/Routes/AppRoutes.jsx
+++ Frontend/src/Routes/AppRoutes.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { AppLayout } from "../Layout/AppLayout";
+
 import Dashboard from "../Pages/Dashboard";
 import Profile from "../Pages/Profile";
+import Mentors from "../Pages/Mentors";
+import Notifications from "../Pages/Notifications";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Dashboard />} />
+        <Route path="/mentors" element={<Mentors />} />
+        <Route path="/notifications" element={<Notifications />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
     </Routes>
